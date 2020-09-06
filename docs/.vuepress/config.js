73438ab348ec655929a5cedff1da6670d6865a47
qxoffice2008 <qxoffice2008@163.com>
2020-09-07 07:04:14 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: 'Chen\'s blog',
-    description: '我的个人网站',
+    title:'清香雨工作室',// 'Chen\'s blog',
+    description: '我的个人博客',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
       // 配置PWA
@@ -10,7 +10,7 @@ module.exports = {
     serviceWorker: true, // 是否开启 PWA
     base: '/', // 这是部署到github相关的配置
     markdown: {
-      lineNumbers: false // 代码块显示行号
+      lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
       nav:[ // 导航栏配置
